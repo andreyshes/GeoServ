7032e50a023f06e57a3e7eb8a827a1ab2d3d558d
Andrey Shestopal <andreyshestopal@Andreys-MacBook-Pro.local>
2025-10-30 15:25:47 -0700

    supabase client

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,3 +1,7 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClientComponentClient();
+export const supabase = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+	{ realtime: { params: { eventsPerSecond: 10 } } }
+);
