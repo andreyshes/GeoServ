13e0099494934a3e2d7a677748eeddd352401937
Andrey Shestopal <andreyshestopal@Andreys-MacBook-Pro.local>
2025-12-05 19:16:14 -0800

    error

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-	apiVersion: "2025-09-30.clover",
+	apiVersion: "2025-10-29.clover",
 });
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
